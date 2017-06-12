c4ea6fef9afc54e1f0110b4f18a8096c646f7abf
gjblanco <gjblanco92@gmail.com>
2017-06-12 17:30:11 -0500

    add more tests

--- tests.test.js
+++ tests.test.js
@@ -5,6 +5,26 @@ test('this.winner() should find winner in diagonal', function(){
   t.board = [['X',,], [,'X',],[,,'X']];
   expect(t.winner()).toBe('X');
 });
+test('this.winner() should find winner in row', function(){
+  var t = new ttt();
+  t.board = [['X',,],['X',,],['X',,]];
+  expect(t.winner()).toBe('X');
+});
+test('this.turn should return X when the board is empty', function(){
+  var t = new ttt();
+  expect(t.turn()).toBe('X');
+});
+test('this.turn should return O if one move has been made', function(){
+  var t = new ttt();
+  t.addMove(0,0)
+  expect(t.turn()).toBe('O');
+});
+// test('adds 1 + 2 to equal 3', function(){
+
+// });
+// test('adds 1 + 2 to equal 3', function(){
+
+// });
 // test('adds 1 + 2 to equal 3', function(){
 
 // });
