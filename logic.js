cbc283dbf25c1c9b9ce1937bc2162e83c71a1a67
gjblanco <gjblanco92@gmail.com>
2017-06-12 17:10:52 -0500

    implement tictactoe

---
+++ logic.js
@@ -0,0 +1,54 @@
+var TTTGame = function() {
+  this.board = [[,,],[,,],[,,]]; // 3 x 3 undefined
+  this.positions = undefined; // array of positions [board, winner]
+};
+
+TTTGame.prototype.winner = function(){
+  for(var i = 0; i < 3; i++) {
+    if(this.board[i][0] === this.board[i][1] && this.board[i][1] === this.board[i][2])
+      return this.board[i][0];
+    if(this.board[0][i] === this.board[1][i] && this.board[1][i] === this.board[2][i])
+      return this.board[0][i]; 
+  }
+  if(this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2])
+    return this.board[0][0];
+  if(this.board[0][2] === this.board[1][1] && this.board[1][1] === this.board[2][0])
+    return this.board[0][2];
+  return undefined;
+};
+
+TTTGame.prototype.turn = function() {
+  var x = 0, o = 0;
+  for(var i = 0; i < 3; i++)
+    for(var j = 0; j < 3; j++) {
+      x += (this.board[i][j] === 'X'? 1 : 0);
+      o += (this.board[i][j] === 'O'? 1 : 0);
+    }
+  return x < o? 'O' : 'X';
+}
+
+TTTGame.prototype.addMove = function(i, j) {
+  if(this.board[i] === undefined || this.board[i][j] !== undefined) {
+    throw 'Invalid Move';
+  }
+  var turn = this.turn();
+  this.board[i][j] = turn;
+  if(this.winner() === turn) {
+    return this.turn() + ' won!';
+  }
+  else return this.turn() + '\'s turn';
+} 
+
+TTTGame.prototype.getBoard = function() {
+  return [this.board[0].slice(), this.board[1].slice(), this.board[2].slice()];
+}
+
+module.exports = TTTGame;
+
+// TTTGame.prototype.move = function() {
+//   var turn = this.turn();
+//   var enemy = (turn === 'X'? 'O' : 'X');
+//   if(this.positions === undefined) {
+//     for(var )
+//   }
+// }
---
+++ ui.js
@@ -0,0 +1,58 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+var TTTGame = require('./logic');
+
+var stdin = process.openStdin();
+
+var ui = {
+  game: new TTTGame(),
+  stateMessage: 'X\'s turn',
+  errorMessage: '',
+  drawBoard: function() {
+    var board = this.game.getBoard();
+    for(var i = 0; i < 3; i++) {
+      var line = '';
+      for(var j = 0; j < 3; j++) {
+        line += (board[i][j] === undefined? ' ' : board[i][j]);
+      }
+      console.log(line);
+    }
+  },
+  drawGameState: function() {
+    process.stdout.write('\x1Bc'); 
+    this.drawBoard();
+    console.log(this.stateMessage);
+    console.log(this.errorMessage);
+    if(!this.stateMessage.includes('won')) {
+      console.log('Insert a valid move for', ui.game.turn(), 
+        ' in the format "row, column" without the quotes');
+    } else {
+      console.log('starting new game...')
+      setTimeout(function(){
+        ui.game = new TTTGame();
+        ui.stateMessage = 'X\'s turn';
+        ui.errorMessage = '';
+        ui.drawGameState();
+      }, 5000)
+    }
+  },
+  addMove: function(i, j) {
+    try {
+      var message = ui.game.addMove(i, j);
+      ui.stateMessage = message;
+    } catch(err) {
+      ui.errorMessage = (err);
+    }
+    ui.drawGameState();
+  }
+};
+
+
+ui.drawGameState();
+
+stdin.addListener('data', function(d) {
+  var input = d.toString().trim().split(',').map(function(mov) { return mov.trim(); });
+  var message;
+  ui.addMove(input[0], input[1]);
+})
